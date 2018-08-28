adf8aef29904d07c427fa899b05fed9fc0e96a25
Trong Vu <vungoctrong@gmail.com>
2018-08-28 13:35:42 +0700

    Update generator.sh
    Using parameter instead of hard-coded name

--- generator.sh
+++ generator.sh
@@ -3,31 +3,30 @@ VERSION=0.1
 AUTHOR=vungoctrong@gmail.com
 
 DATE=`date '+%Y-%m-%d_%H%M%S'`
-APP_NAME=MyApp
 #build xarchive file
-xcodebuild -workspace $APP_NAME.xcworkspace -scheme $APP_NAME -sdk iphoneos archive -archivePath $APP_NAME.xcarchive
+xcodebuild -workspace $1.xcworkspace -scheme $1 -sdk iphoneos archive -archivePath $1.xcarchive
 
 #build IPA file with development provisioning file
-xcodebuild -exportArchive -archivePath $APP_NAME*.xcarchive -exportPath tmp -exportOptionsPlist ExportOptions.plist
+xcodebuild -exportArchive -archivePath $1*.xcarchive -exportPath tmp -exportOptionsPlist ExportOptions.plist
 
 #re-generate IPA file
 cd tmp
-mv $APP_NAME.ipa $APP_NAME.zip
-unzip -qo $APP_NAME.zip
-rm -rf $APP_NAME.zip
+mv $1.ipa $1.zip
+unzip -qo $1.zip
+rm -rf $1.zip
 
 #adding Swift support folder (copy from xarchive file to IPA)
-cp -R ../$APP_NAME*.xcarchive/SwiftSupport .
+cp -R ../$1*.xcarchive/SwiftSupport .
 #replace Frameworks folder
 rm -rf Payload/*.app/Frameworks/
-cp -R ../$APP_NAME*.xcarchive/Products/Applications/*.app/Frameworks Payload/*.app/
+cp -R ../$1*.xcarchive/Products/Applications/*.app/Frameworks Payload/*.app/
 
 #zipping
-zip -qr $APP_NAME.zip Payload SwiftSupport
-mv $APP_NAME.zip ../${APP_NAME}_${DATE}.ipa
+zip -qr $1.zip Payload SwiftSupport
+mv $1.zip ../$1_${DATE}.ipa
 
 cd ..
-echo "IPA file @ `pwd`/${APP_NAME}_${DATE}.ipa"
+echo "IPA file @ `pwd`/$1_${DATE}.ipa"
 rm -rf *.xcarchive
 rm -rf tmp
 echo "**IPA GENERATED**"
